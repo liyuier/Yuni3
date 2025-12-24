e2514faa76f2d06e424a4bd7d94680a036a5c740
liyuier <me@yuier.com>
2025-12-24 17:44:59 +0800

    为插件实例添加一个是否默认开启的字段

--- yuni-plugin/src/main/java/com/yuier/yuni/plugin/model/PluginMetadata.java
+++ yuni-plugin/src/main/java/com/yuier/yuni/plugin/model/PluginMetadata.java
@@ -14,10 +14,32 @@ import java.util.List;
 
 @Data
 public class PluginMetadata {
+    /**
+     * 插件 ID，当前实现为 模块名-插件全限定名
+     */
     private String id;
+    /**
+     * 插件名称，显示给用户
+     */
     private String name;
+    /**
+     * 插件版本
+     */
     private String version;
+    /**
+     * 插件描述，会提供给用户
+     */
     private String description;
+    /**
+     * 插件作者。尊重知识版权说是
+     */
     private String author;
+    /**
+     * 插件依赖的插件 ID ，实际上目前并没有实现依赖检查() TODO
+     */
     private List<String> dependencies;
+    /**
+     * 插件默认是否启用 true 为默认启用，false 为默认禁用
+     */
+    private Boolean defaultEnabled;
 }
