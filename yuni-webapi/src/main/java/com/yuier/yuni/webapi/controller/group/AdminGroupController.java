c2e705a78072fe638929ad1a5c7a9607e200f503
liyuier <me@yuier.com>
2026-06-13 21:58:32 +0800

    消息请求页面

--- yuni-webapi/src/main/java/com/yuier/yuni/webapi/controller/group/AdminGroupController.java
+++ yuni-webapi/src/main/java/com/yuier/yuni/webapi/controller/group/AdminGroupController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 /**
  * 群组管理接口。
  */
@@ -58,4 +57,5 @@ public class AdminGroupController {
         }
         return Result.ok(groupService.getPluginStatus(req.getGroupId()));
     }
+
 }
